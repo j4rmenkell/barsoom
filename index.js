195cb6ac6105a5f68c515f8e9f6f0f0dae6279ca
Johann Virgil Batiancila <jvbatiancila24@gmail.com>
2026-06-17 07:24:00 +0000

    new ver

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
-const nameToGreet = core.getInput('who-to-greet');
-const output_value = `Hello ${nameToGreet}` 
-core.setOutput ("greeting", output_value)
-
+  const nameToGreet = core.getInput('who-to-greet');
+  const output_value = `Hello ${nameToGreet}`;
+  core.setOutput("greeting", output_value);
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
