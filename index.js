e88b6f3760fafcf91f8a882813f933991cf03cba
Johann Virgil Batiancila <jvbatiancila24@gmail.com>
2026-06-17 06:58:55 +0000

    Initial Version

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import * as core from '@actions/core';
+import * as core from '@actions/github';
 
+
+try {
 const nameToGreet = core.getInput('who-to-greet');
-const output_value = `Hello ${nameToGreet}`
-
+const output_value = `Hello ${nameToGreet}` 
 core.setOutput ("greeting", output_value)
+
+} catch (error) {
+    core.setFailed(error.message);
+}
